4352027a278e70d610257412de987f0db74d7d10
hiramcastrod <hiramc@live.com.mx>
2018-08-13 01:54:46 -0600

    initial remote commit

---
+++ app/src/main/java/qr/hiram/qreader/fragments/ScannerFragment.java
@@ -0,0 +1,28 @@
+package qr.hiram.qreader.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import qr.hiram.qreader.R;
+
+public class ScannerFragment extends Fragment {
+
+    public ScannerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scanner, container, false);
+    }
+
+
+
+}
